e7998f16c838e098247ebfbacdd7445bf5975042
Darko Tasevski <darktasevski@gmail.com>
2018-07-22 01:26:06 +0200

    Add Stocks

--- src/components/Stocks/Stocks.vue
+++ src/components/Stocks/Stocks.vue
@@ -1,11 +1,26 @@
 <template>
-	<h1>Stocks Stock</h1>
+	<div>
+		<app-stock v-for="stock in stocks" :key="stock.id"></app-stock>
+	</div>
 </template>
 
 <script>
+import Stock from './Stock.vue'
 
 export default {
-
+	data(){
+		return {
+			stocks: [
+				{id: 1, name: 'ABC', price: 110},
+				{id: 3, name: 'Google', price: 130},
+				{id: 41, name: 'T-Mobile', price: 80},
+				{id: 11, name: 'Startek', price: 210},
+			]
+		}
+	},
+	components: {
+		appStock: Stock
+	}
 }
 </script>
 
