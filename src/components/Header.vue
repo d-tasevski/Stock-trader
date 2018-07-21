92e9d42004a02bf1391a35f8648112c2bca1908c
Darko Tasevski <darktasevski@gmail.com>
2018-07-22 01:16:47 +0200

    Add Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,37 @@
 <template>
-	<h1>Header</h1>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+		<div class="collapse navbar-collapse">
+			<ul class="navbar-nav">
+				<router-link to="/portfolio" activeClass="active" tag="li">
+					<a class="nav-link">Portfolio</a>
+				</router-link>
+				<router-link to="/stocks" activeClass="active" tag="li">
+					<a class="nav-link">Stocks</a>
+				</router-link>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a class="nav-link dropdown-toggle dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Save & Load
+						<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<li>
+							<a href="#" @click="saveData">Save Data</a>
+						</li>
+						<li>
+							<a href="#" @click="loadData">Load Data</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<strong class="navbar-text nav-link navbar-right">End Day</strong>
+		<strong class="navbar-text navbar-right">Funds: 1000 $</strong>
+	</nav>
 </template>
 
 <script>
